1fe1d0af255574921264d99f8519fc04fb04a9ee
Nils Fahldieck <nils.fahldieck@hashicorp.com>
2023-06-16 17:43:35 +0200

    remove output

--- outputs.tf
+++ outputs.tf
@@ -6,10 +6,6 @@ output "catapp_url" {
   value = "http://${azurerm_public_ip.catapp-pip.fqdn}"
 }
 
-output "catapp_url2" {
-  value = "http://${data.azurerm_public_ip.example.domain_name_label}"
-}
-
 output "catapp_ip" {
   value = "http://${data.azurerm_public_ip.example.ip_address}"
 }
